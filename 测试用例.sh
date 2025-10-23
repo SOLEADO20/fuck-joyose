c1ffa9954a7f9dc25f759463150adefce04c6433
SOLEADO20 <96033686+SOLEADO20@users.noreply.github.com>
2025-10-24 01:43:39 +0800

    Updated.

--- 测试用例.sh
+++ 测试用例.sh
@@ -1,3 +1,4 @@
 # 测试用例
 bash "/storage/emulated/0/001_中转站/fuck-joyose/fuck-joyose.sh"
 bash "/storage/emulated/0/Documents/fuck-joyose/fuck-joyose.sh"
+bash "/storage/emulated/0/101o5_自用工具/fuck-joyose/fuck-joyose.sh"
